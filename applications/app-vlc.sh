35aeb4197642337b494b0bc865b4090b0046b9d4
npesaras <nilmar.pesarast@gmail.com>
2025-11-22 08:34:31 +0800

    update vlc.sh

--- applications/app-vlc.sh
+++ applications/app-vlc.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 # Check if VLC is already installed
-if command -v vlc &> /dev/null; then
+if command -v vlc > /dev/null 2>&1; then
     echo "VLC is already installed, skipping..."
     exit 0
 fi
