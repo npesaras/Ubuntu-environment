a965f22949e68ad83ea093e921b109512c18f4b6
npesaras <nilmar.pesarast@gmail.com>
2025-11-22 08:33:54 +0800

    update lazygit.sh

--- applications/app-lazy-git.sh
+++ applications/app-lazy-git.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 # Check if lazygit is already installed
-if command -v lazygit &> /dev/null; then
+if command -v lazygit > /dev/null 2>&1; then
     echo "lazygit is already installed, skipping..."
     exit 0
 fi
