71de03c255638cc4a92b2d5ad1a22f331f1b0300
npesaras <nilmar.pesarast@gmail.com>
2025-11-22 08:33:47 +0800

    update git.sh

--- applications/app-git.sh
+++ applications/app-git.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 # Check if Git is already installed
-if command -v git &> /dev/null; then
+if command -v git > /dev/null 2>&1; then
     echo "Git is already installed, skipping..."
     exit 0
 fi
