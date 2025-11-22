11e70fd3d63ba575d2b433667f0acf9b0f1ff8a9
npesaras <nilmar.pesarast@gmail.com>
2025-11-22 17:29:19 +0800

    update config brave.sh

--- applications/app-brave.sh
+++ applications/app-brave.sh
@@ -8,13 +8,9 @@ if command -v brave-browser > /dev/null 2>&1; then
     exit 0
 fi
 
-sudo apt install curl
-
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
 sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
-
-sudo apt update -qq
 
 sudo apt install -y brave-browser
 
