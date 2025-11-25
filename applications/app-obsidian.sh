52ef68952e9708ce4251c0f0e39fe6cb1a76e9f6
npesaras <nilmar.pesarast@gmail.com>
2025-11-25 13:29:07 +0800

    change wget to curl

--- applications/app-obsidian.sh
+++ applications/app-obsidian.sh
@@ -10,7 +10,7 @@ cd /tmp
 
 OBSIDIAN_VERSION=$(curl -s https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | grep -Po '"tag_name": "v\K[^"]*')
 
-wget -O obsidian.deb "https://github.com/obsidianmd/obsidian-releases/releases/latest/download/obsidian_${OBSIDIAN_VERSION}_amd64.deb"
+curl -Lo obsidian.deb "https://github.com/obsidianmd/obsidian-releases/releases/latest/download/obsidian_${OBSIDIAN_VERSION}_amd64.deb"
 
 sudo apt install -y ./obsidian.deb
 
