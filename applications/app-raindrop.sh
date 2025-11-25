f0b959dde44d650f9a65d44dd4dc60ebfbdc1c50
npesaras <nilmar.pesarast@gmail.com>
2025-11-25 13:05:17 +0800

    move raindrop

--- applications/app-raindrop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if command -v raindrop > /dev/null 2>&1; then
-    echo "Raindrop is already installed, skipping..."
-    exit 0
-fi
-
-# Install Raindrop via snap
-sudo snap install raindrop
