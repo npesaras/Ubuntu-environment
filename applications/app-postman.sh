4fcbfff20a194cd8e838aaeb994e28af4168e963
npesaras <nilmar.pesarast@gmail.com>
2025-11-25 13:20:12 +0800

    use snap instead of curl

--- applications/app-postman.sh
+++ applications/app-postman.sh
@@ -7,16 +7,3 @@ fi
 
 # Install Postman via snap
 sudo snap install postman
-
-cd /tmp
-
-echo "Installing Postman..."
-
-curl -fsSLo postman-latest.deb https://dl.pstmn.io/download/latest/linux_64
-
-sudo apt install -y ./postman-latest.deb
-
-rm postman-latest.deb
-
-cd -
-
