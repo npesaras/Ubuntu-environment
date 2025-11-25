83ac18c9356750e2b5d8b7f38770dbed077aa25c
npesaras <nilmar.pesarast@gmail.com>
2025-11-25 11:34:03 +0800

    move docker-engine to optional

---
+++ applications/optional/app-docker-engine.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+if ! command -v docker > /dev/null 2>&1; then
+    echo "Docker is not installed"
+else
+    echo "Docker is installed"
+fi
+
+
+printf "Do you want to install Docker? (y/N): "
+read -r REPLY
+echo
+
+if [ "$REPLY" != "y" ] && [ "$REPLY" != "Y" ]; then
+    echo "Skipping Docker installation..."
+    exit 0
+fi
+
+echo "Installing Docker..."
+
+# Add Docker's official GPG key:
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+# Install Docker Engine, CLI, and Containerd
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Give this user privileged Docker access
+sudo usermod -aG docker ${USER}
+
+# Limit log size to avoid running out of disk
+echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
